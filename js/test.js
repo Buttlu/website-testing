dd7c0ebaf9e9c7674812e79ce9621bc3f3b535e4
Buttlu <jackan202@gmail.com>
2022-07-26 00:30:07 +0200

    more react stuff I don't understand

--- js/test.js
+++ js/test.js
@@ -1,10 +1,26 @@
-import React from 'React';
-import ReactDOM from 'react-dom';
+'use strict';
 
-class TestClass extends React.Component() {
-    render () {
-        return <h1>Hello There</h1>;
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
     }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
 }
 
-ReactDOM.render(<TestClass/>, document.getElementById('test.js'));
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
